ee7362b15b98bc7e6dba0dcb66bd1f72aea059e6
mounika-ellorum <mounika.gollu@ellorum.com>
2021-02-24 20:08:01 +0530

    Add learning app

---
+++ learning-app/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources';
+
+
+export default {
+  components: {
+    StoredResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        { 
+          id: '1', 
+          title: 'Official Guide', 
+          description: 'The official Vue.js documentation',
+          link: '/'
+        },
+        { 
+          id: '2', 
+          title: 'Google', 
+          description: 'Learn to google...',
+          link: '/'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ learning-app/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul>
+    <learning-resource v-for=" res in resources" :key="res.id"
+    :title="res.title"
+    :description="res.description"
+    :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource';
+
+export default {
+    components: {
+    LearningResource
+    },
+    props: ['resources'], 
+}
+</script>
