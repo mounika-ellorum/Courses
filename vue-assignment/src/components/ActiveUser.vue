95d4b14fb03394344fc2f9f90b7a4564b0045d3b
mounika-ellorum <mounika.gollu@ellorum.com>
2021-02-24 16:55:21 +0530

    Add assignment six

---
+++ vue-assignment/src/components/ActiveUser.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <h2>{{name}}</h2>
+        <h2>{{age}} years</h2>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        age: Number
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-assignment/src/components/UserData.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <form @submit.prevent="submitData">
+            <input type="text" placeholder="name" v-model="enteredName"/>
+            <input type="text" placeholder="age" v-model="enteredAge"/>
+            <button>set user data</button>
+        </form>
+    </section>  
+</template>
+
+<script>
+export default {
+    emits: ['set-data'],
+    data() {
+        return {
+            enteredName: '',
+            enteredAge: ''
+        }
+    },
+    submitData() {
+        this.$emit('set-data', this.enteredName, this.enteredAge);
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
